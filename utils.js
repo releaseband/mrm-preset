5620ebb602c5363fd9da0c941b83a308e045a9b1
Denis <denis@releaseband.com>
2022-02-03 17:55:04 +0300

    fix: fix export in utils

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 
 // eslint-disable-next-line import/prefer-default-export
-export function installPeerDeps(pkg) {
+module.exports.installPeerDeps = function installPeerDeps(pkg) {
   execSync(`npx install-peerdeps --dev ${pkg}`, { stdio: 'inherit' });
-}
+};
